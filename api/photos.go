6898bee0ddec53d81bb288de9f9be91e534026db
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2016-12-12 06:30:25 +0530

    Fetching all the photos

--- api/photos.go
+++ api/photos.go
@@ -7,10 +7,12 @@ import (
 	"github.com/headzoo/surf"
 )
 
-func main() {
+var ct int = 0
+
+func page_iterator(uri string) {
 	//initiate browser
 	bow := surf.NewBrowser()
-	bow.Open("https://www.instagram.com/shubhothegreat")
+	bow.Open(uri)
 
 	scr := bow.Find("script").Eq(6)
 
@@ -23,8 +25,26 @@ func main() {
 	//posts
 	posts, _ := parse["nodes"].([]interface{})
 
-	for i := range posts {
-		post_uri, _ := posts[i].(map[string]interface{})["display_src"].(string)
-		fmt.Println(post_uri)
+	// for i := range posts {
+	// 	post_uri, _ := posts[i].(map[string]interface{})["display_src"].(string)
+	// 	fmt.Println(post_uri)
+	// }
+
+	ct = ct + len(posts)
+	//next_page
+	page_info, _ := parse["page_info"].(map[string]interface{})
+	hasnextpage, _ := page_info["has_next_page"].(bool)
+
+	fmt.Println(hasnextpage)
+
+	if hasnextpage {
+		end_cursor := page_info["end_cursor"].(string)
+		fmt.Println(page_info)
+		page_iterator(uri + "/?max_id=" + end_cursor)
 	}
+
+}
+
+func main() {
+	page_iterator("https://www.instagram.com/rishi_raj95")
 }
