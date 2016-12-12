7a6d9a8d6c92cc468e06c26f1168e42df484209b
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2016-12-12 06:46:57 +0530

    package done

--- api/photos.go
+++ api/photos.go
@@ -3,16 +3,17 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	_ "github.com/PuerkitoBio/goquery"
 	"github.com/headzoo/surf"
 )
 
 var ct int = 0
+var uri string
+var urls []string
 
-func page_iterator(uri string) {
+func page_iterator(url string) {
 	//initiate browser
 	bow := surf.NewBrowser()
-	bow.Open(uri)
+	bow.Open(url)
 
 	scr := bow.Find("script").Eq(6)
 
@@ -25,26 +26,24 @@ func page_iterator(uri string) {
 	//posts
 	posts, _ := parse["nodes"].([]interface{})
 
-	// for i := range posts {
-	// 	post_uri, _ := posts[i].(map[string]interface{})["display_src"].(string)
-	// 	fmt.Println(post_uri)
-	// }
+	for i := range posts {
+		post_uri, _ := posts[i].(map[string]interface{})["display_src"].(string)
+		urls = append(urls, post_uri)
+	}
 
-	ct = ct + len(posts)
 	//next_page
 	page_info, _ := parse["page_info"].(map[string]interface{})
 	hasnextpage, _ := page_info["has_next_page"].(bool)
 
-	fmt.Println(hasnextpage)
-
 	if hasnextpage {
 		end_cursor := page_info["end_cursor"].(string)
-		fmt.Println(page_info)
 		page_iterator(uri + "/?max_id=" + end_cursor)
 	}
 
 }
 
 func main() {
-	page_iterator("https://www.instagram.com/rishi_raj95")
+	uri = "https://www.instagram.com/rishi_raj95"
+	page_iterator(uri)
+	fmt.Println(len(urls))
 }
